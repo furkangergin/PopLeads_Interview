eeeec64365bd22debec2b3e04e206b5539c8b9f4
furkangergin <hunter_1325@hotmail.com>
2023-11-23 12:41:55 +0300

    kasım last

--- src/test/java/com/popLeads/stepDefinition/Hooks.java
+++ src/test/java/com/popLeads/stepDefinition/Hooks.java
@@ -14,7 +14,7 @@ public class Hooks {
     public void sentResult(Scenario scenario) {
         String result = scenario.getStatus().name();
         System.out.println("result = " + result);
-        ExcelHelper.writeExcel(scenario.getName() + " " + result + " olarak sonuçlandı.");
+        ExcelHelper.writeExcel(scenario.getName() + " " + result + " result.");
          //DriverFactory.closeDriver();
     }
 
